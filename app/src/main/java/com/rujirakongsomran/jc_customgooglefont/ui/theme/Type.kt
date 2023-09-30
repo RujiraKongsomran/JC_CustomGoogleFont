bdc1bbe723ea77053aae0f9472ee9353ee114105
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-30 12:55:01 +0700

    Define a FontFamily

--- app/src/main/java/com/rujirakongsomran/jc_customgooglefont/ui/theme/Type.kt
+++ app/src/main/java/com/rujirakongsomran/jc_customgooglefont/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.rujirakongsomran.jc_customgooglefont.R
@@ -13,6 +14,13 @@ val provider = GoogleFont.Provider(
     providerPackage = "com.google.android.gms",
     certificates = R.array.com_google_android_gms_fonts_certs
 )
+
+val fontName = GoogleFont("Lobster Two")
+
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
