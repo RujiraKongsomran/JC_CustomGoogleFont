3b7b5816d6e795a3e1a39501bd958c0587501fd2
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-30 13:44:49 +0700

    Debug your implementation

--- app/src/main/java/com/rujirakongsomran/jc_customgooglefont/MainScreen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_customgooglefont/MainScreen.kt
@@ -9,19 +9,34 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rujirakongsomran.jc_customgooglefont.ui.theme.fontFamily
+import kotlinx.coroutines.CoroutineExceptionHandler
+import android.util.Log
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.text.font.createFontFamilyResolver
 
 @Preview(showSystemUi = true)
 @Composable
 fun MainScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Hardin Scott",
-            fontFamily = fontFamily,
-            fontSize = MaterialTheme.typography.displayLarge.fontSize,
-            style = MaterialTheme.typography.bodyMedium
-        )
+    val handler = CoroutineExceptionHandler { _, throwable ->
+        // process the Throwable
+        Log.e("MainScreen", "There has been an issue: ", throwable)
     }
+    CompositionLocalProvider(
+        LocalFontFamilyResolver provides createFontFamilyResolver(LocalContext.current, handler)
+    ){
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Hardin Scott",
+                fontFamily = fontFamily,
+                fontSize = MaterialTheme.typography.displayLarge.fontSize,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+
 }
